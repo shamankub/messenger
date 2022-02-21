fbd0dca7acfb57a27cf13489a47edcf2691f6015
Антон Есенеев <84543711+shamankub@users.noreply.github.com>
2022-02-21 16:41:45 +0300

    Lesson 2

---
+++ Lesson_2_Eseneev/task_2/task_2.py
@@ -0,0 +1,65 @@
+"""
+2. Задание на закрепление знаний по модулю json. Есть файл orders
+в формате JSON с информацией о заказах. Написать скрипт, автоматизирующий
+его заполнение данными.
+
+Для этого:
+Создать функцию write_order_to_json(), в которую передается
+5 параметров — товар (item), количество (quantity), цена (price),
+покупатель (buyer), дата (date). Функция должна предусматривать запись
+данных в виде словаря в файл orders.json. При записи данных указать
+величину отступа в 4 пробельных символа;
+Проверить работу программы через вызов функции write_order_to_json()
+с передачей в нее значений каждого параметра.
+
+ПРОШУ ВАС НЕ УДАЛЯТЬ ИСХОДНЫЙ JSON-ФАЙЛ
+ПРИМЕР ТОГО, ЧТО ДОЛЖНО ПОЛУЧИТЬСЯ
+
+{
+    "orders": [
+        {
+            "item": "printer",
+            "quantity": "10",
+            "price": "6700",
+            "buyer": "Ivanov I.I.",
+            "date": "24.09.2017"
+        },
+        {
+            "item": "scaner",
+            "quantity": "20",
+            "price": "10000",
+            "buyer": "Petrov P.P.",
+            "date": "11.01.2018"
+        }
+    ]
+}
+
+вам нужно подгрузить JSON-объект
+и достучаться до списка, который и нужно пополнять
+а потом сохранять все в файл
+"""
+
+import json
+
+
+def write_order_to_json(item, quantity, price, buyer, date):
+    with open("./task_2/orders.json", "r", encoding="utf-8") as f:
+        text = json.load(f)
+
+    with open("./task_2/orders.json", "w", encoding="utf-8") as f:
+        dump_list = text["orders"]
+        dict = {
+            "item": item,
+            "quantity": quantity,
+            "price": price,
+            "buyer": buyer,
+            "date": date,
+        }
+        dump_list.append(dict)
+        json.dump(text, f, indent=4, ensure_ascii=False)
+
+
+write_order_to_json("printer", "10", "6700", "Ivanov I.I.", "24.09.2017")
+write_order_to_json("scaner", "20", "10000", "Petrov P.P.", "11.01.2018")
+write_order_to_json("принтер", "10", "6700", "Ivanov I.I.", "24.09.2017")
+write_order_to_json("сканер", "20", "10000", "Petrov P.P.", "11.01.2018")
