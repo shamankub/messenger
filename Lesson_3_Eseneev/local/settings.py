20b4c7af9a1f1d04c84c92b4044326db15b0f8e9
Shamankub <shaman_kub@mail.ru>
2022-02-25 22:59:40 +0300

    Lesson 3. All done.

---
+++ Lesson_3_Eseneev/local/settings.py
@@ -0,0 +1,15 @@
+# Параметры соединения:
+DEFAULT_PORT = 7777
+DEFAULT_IP = "localhost"
+MAX_CONNECTIONS = 5
+MAX_PACKAGE_LENGTH = 1024
+
+# Протокол JIM:
+ACTION = "action"
+TIME = "time"
+USER = "user"
+ACCOUNT_NAME = "account_name"
+
+PRESENCE = "presence"
+RESPONSE = "response"
+ERROR = "error"
