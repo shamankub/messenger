ba813d7f09dabafd9970487a16a763a74974c540
Антон Есенеев <84543711+shamankub@users.noreply.github.com>
2022-04-01 22:40:46 +0300

    Lesson 8. All done. (#8)

---
+++ Lesson_8_Eseneev/local/settings.py
@@ -0,0 +1,20 @@
+# Параметры соединения:
+DEFAULT_PORT = 7777
+DEFAULT_IP = "localhost"
+MAX_CONNECTIONS = 5
+MAX_PACKAGE_LENGTH = 1024
+
+# Протокол JIM:
+ACTION = "action"
+TIME = "time"
+USER = "user"
+ACCOUNT_NAME = "account_name"
+SENDER = "from"
+DESTINATION = "to"
+
+PRESENCE = "presence"
+RESPONSE = "response"
+ERROR = "error"
+MESSAGE = "message"
+MESSAGE_TEXT = "message_text"
+EXIT = "exit"
